ff3e2db082062c772f948c8e99b0c0386c1bb4ee
Max Miorim <max@miorim.com>
2019-08-02 11:25:31 -0400

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cq "0.3.0"
+(defproject cq "0.4.0-SNAPSHOT"
   :description "Command line tool to interact with EDN data/files"
   :main cq.cli
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.cli "0.4.1"]
-                 [zprint "0.4.15"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.cli "0.4.2"]
+                 [zprint "0.4.16"]]
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[lein-binplus "0.6.4"]]}}
+             :dev     {:plugins [[lein-binplus "0.6.4"]]}}
   :bin {:bootclasspath true})
