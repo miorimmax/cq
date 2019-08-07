9551ce66d49c2f33fce4209817401cfbdaf6e179
Max Miorim <max@miorim.com>
2019-08-07 19:19:36 -0400

    Set up dependency exclusions

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject cq "0.4.0"
   :description "Command line tool to interact with EDN data/files"
   :main cq.cli
+  :exclusions [org.clojure/clojure]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "0.4.2"]
-                 [zprint "0.4.16"]]
+                 [zprint "0.4.16" :exclusions [org.clojure/tools.reader]]]
   :profiles {:uberjar {:aot :all}
              :kaocha  {:dependencies [[lambdaisland/kaocha "0.0-529"]]}
-             :dev     {:dependencies [[mockfn "0.4.0"]
-                                      [nubank/matcher-combinators "1.0.1"]]
+             :dev     {:dependencies [[mockfn "0.4.0" :exclusions [midje]]
+                                      [nubank/matcher-combinators "1.0.1" :exclusions [midje]]]
                        :plugins      [[lein-binplus "0.6.4"]
                                       [lein-cljfmt "0.6.4"]]}}
   :bin {:name          "cq"
