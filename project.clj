ce9ed9b40296be899316c207399ffc3d624f833a
Max Miorim <max@miorim.com>
2019-08-07 20:21:55 -0400

    Fix dependency issues

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :exclusions [org.clojure/clojure]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "0.4.2"]
-                 [zprint "0.4.16" :exclusions [org.clojure/tools.reader]]]
+                 [zprint "0.4.16"]]
   :profiles {:uberjar {:aot :all}
              :kaocha  {:dependencies [[lambdaisland/kaocha "0.0-529"]]}
              :dev     {:dependencies [[mockfn "0.4.0" :exclusions [midje]]
                                       [nubank/matcher-combinators "1.0.1" :exclusions [midje]]]
-                       :plugins      [[lein-binplus "0.6.4"]
+                       :plugins      [[lein-binplus "0.6.5"]
                                       [lein-cljfmt "0.6.4"]]}}
   :bin {:name          "cq"
         :bootclasspath true}
